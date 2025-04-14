eeefc16fb4a37ebe20ddee493fd88a3a9fa8b45e
Calin-Andrei Rasiga <rasigacalin@gmail.com>
2025-04-14 18:55:39 +0200

    I: Added the ParentNode class which has a required tag and children. Sevelra unit tests were created

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -34,3 +34,20 @@ class LeafNode(HTMLNode):
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children,props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no value")
+        elif self.children is None:
+            raise ValueError("invalid HTML: missing children")
+        html=""
+        for child in self.children:
+            html += f"<{self.tag}>{child.to_html()}</{self.tag}>"
+        return html
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+
