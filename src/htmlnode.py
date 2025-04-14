3183e4ce4917d9c8a8902a4e40def1292ff6c7af
Calin-Andrei Rasiga <rasigacalin@gmail.com>
2025-04-14 18:39:57 +0200

    H: Refactored the leaf node code and added additional tests.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,18 +1,36 @@
 class HTMLNode:
-    def __init__(self, tag = None, value = None, children = None, props = None):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError()
+        raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
-        aggregator = ""
+        if self.props is None:
+            return ""
+        props_html = ""
         for prop in self.props:
-            aggregator += f"{prop}={self.props[prop]}\n"
-        return aggregator
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
 
     def __repr__(self):
-        return f"{self.tag}\n{self.value}\n{self.children}\n{self.props_to_html()}"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
